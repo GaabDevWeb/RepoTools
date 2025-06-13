c759641d31b4f10386586b40b2bf17ee608753ee
Gaab <154674155+GaabDevWeb@users.noreply.github.com>
2025-06-13 20:04:47 -0300

    Multiplayer

---
+++ js/multiplayer/syncSala.js
@@ -0,0 +1,5 @@
+// Módulo de sincronização multiplayer (placeholder)
+export function iniciarSyncSala(codigoSala) {
+  // Implemente a sincronização aqui
+  console.log("Sincronização multiplayer ativada para sala:", codigoSala);
+}
